34ca3590358ea24f4cc91ecc129982325e3c6da4
JiaDingYi <wzy2010416033@163.com>
2019-06-14 14:46:16 +0800

    set up ui

--- Example/testForKwalee/YumiViewController.m
+++ Example/testForKwalee/YumiViewController.m
@@ -18,6 +18,46 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+- (void)setUpUI {
+    YumiMobileTools *tool = [YumiMobileTools sharedTool];
+    CGFloat buttonWidth = [tool adaptedValue6:200];
+    CGFloat buttonHeight = [tool adaptedValue6:50];
+    CGFloat margin = [tool adaptedValue6:10];
+    CGFloat topMargin = [tool adaptedValue6:10];
+    if ([tool isiPhoneX]) {
+        topMargin += kIPHONEXSTATUSBAR;
+    }
+    if ([tool isiPhoneXR]) {
+        topMargin += kIPHONEXRSTATUSBAR;
+    }
+    // request banner
+    self.requestBannerBtn = [[UIButton alloc] initWithFrame:CGRectMake((screenWidth - buttonWidth)/2, topMargin, buttonWidth, buttonHeight)];
+    [self.requestBannerBtn addTarget:self action:@selector(requestBannerAd) forControlEvents:UIControlEventTouchUpInside];
+    self.requestBannerBtn.backgroundColor = [UIColor blackColor];
+    self.requestBannerBtn.layer.cornerRadius = 10;
+    self.requestBannerBtn.layer.masksToBounds = YES;
+    [self.requestBannerBtn setTitle:@"Request Banner" forState:UIControlStateNormal];
+    [self.view addSubview:self.requestBannerBtn];
+    // request interstitial
+    self.requestInterstitialBtn = [[UIButton alloc] initWithFrame:CGRectMake((screenWidth - buttonWidth)/2, topMargin + buttonHeight + margin, buttonWidth, buttonHeight)];
+    [self.requestInterstitialBtn addTarget:self action:@selector(requestInterstitialAd) forControlEvents:UIControlEventTouchUpInside];
+    self.requestInterstitialBtn.backgroundColor = [UIColor blackColor];
+    self.requestInterstitialBtn.layer.cornerRadius = 10;
+    self.requestInterstitialBtn.layer.masksToBounds = YES;
+    [self.requestInterstitialBtn setTitle:@"Request Interstitial" forState:UIControlStateNormal];
+    [self.view addSubview:self.requestInterstitialBtn];
+    // present interstitial
+    self.presentInterstitialBtn = [[UIButton alloc] initWithFrame:CGRectMake((screenWidth - buttonWidth)/2, topMargin + buttonHeight * 2 + margin * 2, buttonWidth, buttonHeight)];
+    [self.presentInterstitialBtn addTarget:self action:@selector(presentInterstitialAd) forControlEvents:UIControlEventTouchUpInside];
+    self.presentInterstitialBtn.backgroundColor = [UIColor blackColor];
+    self.presentInterstitialBtn.layer.cornerRadius = 10;
+    self.presentInterstitialBtn.layer.masksToBounds = YES;
+    [self.presentInterstitialBtn setTitle:@"Present Interstitial" forState:UIControlStateNormal];
+    [self.view addSubview:self.presentInterstitialBtn];
+    self.console = [[UITextView alloc] initWithFrame:CGRectMake(0, topMargin + buttonHeight * 3 + margin * 3, screenWidth, screenHeight*0.7 - 100)];
+    self.console.backgroundColor = [UIColor grayColor];
+    [self.view addSubview:self.console];
+}
 }
 
 - (void)didReceiveMemoryWarning
